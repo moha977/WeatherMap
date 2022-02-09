5518a255ccceaea84f451bab9e91cd9263b72954
Mohammad ahma <ahmad.moh0812@icloud.com>
2022-02-09 15:51:37 -0500

    Finished

---
+++ WeatherMap/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace WeatherMap
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            string key = File.ReadAllText("appsettings.json");
+            var api_key = JObject.Parse(key).GetValue("api_key").ToString();
+            while (true)
+            {
+                Console.WriteLine("Welcome To Weather Map");
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("Enter Your City Name:");
+                var city_name = Console.ReadLine();
+                var weatherURL = $"http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={api_key}";
+                var response = client.GetStringAsync(weatherURL).Result;
+
+                var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+                Console.WriteLine(formattedResponse);
+                Console.WriteLine();
+                Console.WriteLine("Would you like to choose a diffrent city?  Yes/No");
+               var answer = Console.ReadLine();
+                if (answer.ToLower() == "yes")
+                {
+                    Console.WriteLine(formattedResponse);                                                                            
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
